9951ee59d3b958bc93895dd440e726cd51ad238d
Lewis Nixon <lewcfn@gmail.com>
2013-07-17 16:00:11 +0100

    adding the actual plugin

--- src/dropmenu.js
+++ src/dropmenu.js
@@ -1,38 +1,102 @@
-/*
- * dropmenu
- * 
- *
- * Copyright (c) 2013 Lewis Nixon
- * Licensed under the MIT license.
- */
+// see http://coding.smashingmagazine.com/2011/10/11/essential-jquery-Dropmenu-patterns/
+// the semi-colon before the function invocation is a safety 
+// net against concatenated scripts and/or other Dropmenus 
+// that are not closed properly.
+;(function ( $, window, document, undefined ) {
+  
+  // undefined is used here as the undefined global 
+  // variable in ECMAScript 3 and is mutable (i.e. it can 
+  // be changed by someone else). undefined isn't really 
+  // being passed in so we can ensure that its value is 
+  // truly undefined. In ES5, undefined can no longer be 
+  // modified.
+  
+  // window and document are passed through as local 
+  // variables rather than as globals, because this (slightly) 
+  // quickens the resolution process and can be more 
+  // efficiently minified (especially when both are 
+  // regularly referenced in your Dropmenu).
 
-(function ($) {
+  // Create the defaults once
+  var dropmenu = 'dropmenu',
+      defaults = {
+        propertyName: "value"
+      };
 
-  // Collection method.
-  $.fn.awesome = function () {
-    return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  // The actual Dropmenu constructor
+  function Dropmenu( element, options ) {
+
+    this.element = element;
+
+    // jQuery has an extend method that merges the 
+    // contents of two or more objects, storing the 
+    // result in the first object. The first object 
+    // is generally empty because we don't want to alter 
+    // the default options for future instances of the Dropmenu
+    this.options = $.extend( {}, defaults, options) ;
+    
+    this._defaults = defaults;
+    this._name = dropmenu;
+    
+    this.init();
+    this.toggle();
+  }
+
+  Dropmenu.prototype = {
+    
+    init: function () {
+      // Place initialization logic here
+      // You already have access to the DOM element and
+      // the options via the instance, e.g. this.element 
+      // and this.options
+
+      // Grab our child menu, at the moment its hardcoded
+      // attach it's height to the main object
+
+      this.child = $(this.element).find('.navigation');
+      this.child.height = this.getHeight(this.child);
+    },
+    
+    getHeight: function (el) {
+      if (el) {
+        return $(el).height();
+      }
+    },
+
+    toggle: function () {
+      this[$(this.element).hasClass( this.options.toggle ) ? 'show' : 'hide']();      
+    },
+
+    hide: function () {
+      $(this.element).addClass( this.options.toggle ).height(0);      
+    },
+
+    show: function () {
+      $(this.element).removeClass( this.options.toggle ).height( this.child.height );    
+    }
+  };
+
+  $.fn[dropmenu] = function ( options ) {
+    return this.each(function () {
+      new Dropmenu( this, options );
     });
   };
 
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
+ /* COLLAPSE DATA-API
+  * ================= */
 
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
+  $(document).on('click.dropmenu.data-api', '[data-toggle=collapsed]', function (e) {
+  
+    var $this = $(this), 
+    href,
 
-}(jQuery));
+    // regex strip for ie7
+    target = $this.attr('data-target') || e.preventDefault() || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''),
+    option = $this.data();
+    // $(target).toggleClass(option.toggle);
+    $(target).dropmenu(option);
+  });
+
+
+})( jQuery, window, document );
