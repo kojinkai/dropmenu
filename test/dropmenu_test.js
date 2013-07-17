f551d7b152a8816dd4b5913a265218251bf7af81
Lewis Nixon <lewcfn@gmail.com>
2013-07-17 16:00:26 +0100

    updating the tests

--- test/dropmenu_test.js
+++ test/dropmenu_test.js
@@ -20,43 +20,22 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  var $DOM = $( '#qunit-fixture' ),
+      $NAV = $DOM.children('div'),
+      $LIST = $NAV.children('li');
+
+  module('jQuery#dropmenu', {
     // This will run before each test in this module.
     setup: function() {
-      this.elems = $('#qunit-fixture').children();
+      this.elem = $NAV;
+      this.elem.list = $LIST;
     }
   });
 
   test('is chainable', function() {
     expect(1);
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
+    strictEqual(this.elem.dropmenu(), this.elem, 'should be chainable');
   });
 
 }(jQuery));
